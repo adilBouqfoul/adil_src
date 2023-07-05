cb821af11671d47fe62f7f6e6bb682d410eb08e4
adilBouqfoul <138655168+adilBouqfoul@users.noreply.github.com>
2023-07-05 10:07:32 +0000

    fix deploy script

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -6,16 +6,17 @@ echo "executing $0 {"
 
 rm -rf adil_webserver/
 
-git clone  https://github.com/adilBouqfoul/adil_webserver.git
+git clone  "https://$MACHINE_USER_TOKEN@github.com/adilBouqfoul/adil_webserver.git"
 
 cd adil_webserver/
 
 cp ../index.html .
 
 git add --all
-git commit -m "$(date)"
+git commit -m "$(date)" || true
 git push
 
+cd ../
 
 rm -rf adil_webserver/
 echo "done $0 {"
